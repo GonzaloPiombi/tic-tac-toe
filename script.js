7b6b743d3bde9270a6cedf88fc517f8b3782d2ba
Gonzalo <gonzalopiombi@gmail.com>
2021-08-19 12:46:19 -0300

    Add comments for better understanding of how checking for the winner works.

--- script.js
+++ script.js
@@ -18,6 +18,7 @@ const gameBoard = (() => {
 const game = (() => {
     let moves = [];
     let playerMove;
+    //'rows' and 'columns' arrays have 3 values, each one representing one row (from top to bottom) and one column (from left to right).
     const rows = [0, 0, 0];
     const columns = [0, 0, 0];
 
@@ -25,7 +26,8 @@ const game = (() => {
         document.querySelectorAll('.square').forEach(div => {
             div.addEventListener('click', function clicked(e) {
                 squareClicked = e.currentTarget;
-                squareClicked.removeEventListener(e.type, clicked);
+                //Remove event listener because clicking an already occupied square would affect the rows and columns arrays values.
+                squareClicked.removeEventListener(e.type, clicked); 
                 _makeMark(squareClicked);
                 _checkWinner(e.currentTarget.dataset.index);
             });
@@ -58,6 +60,10 @@ const game = (() => {
             [6, 7, 8]
         ];
 
+        //Iterate through the grid array until it finds the index of 'squareIndex'.
+        //If the player was player1 (playerMove === 'x') it will add 1 to the corresponding row in the rows array.
+        //It will subtract 1 if the player was player2 (playerMove === 'o')/ 
+        //If the row array reaches 3 in anyone of its values it means player1 won. If the row array reaches -3 in anyone of its values it means player2 won.
         for(let i = 0; i < grid.length; i++) {
             if (grid[i].indexOf(squareIndex) !== -1) {
                 if (playerMove === 'x') {
@@ -73,6 +79,7 @@ const game = (() => {
             }
         }
 
+        //Check for 'squareIndex' in the grid array so the same process aplied to rows can be aplied to columns.
         for(let i = 0; i < grid.length; i++) {
             if (grid[i].indexOf(squareIndex) !== -1) {
                 if (playerMove === 'x') {
@@ -92,6 +99,8 @@ const game = (() => {
     }
 
     const _checkDiagonals = () => {
+        //Each variable represents a square of the diagonals.
+        //Winning combinations would be squares 0, 4, 8 or 2, 4, 6.
         const d1 = document.querySelectorAll('.square')[0];
         const d2 = document.querySelectorAll('.square')[4];
         const d3 = document.querySelectorAll('.square')[8];

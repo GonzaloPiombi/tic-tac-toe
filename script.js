22fc075a4f1841783686d34a4925380a047d80ef
Gonzalo <gonzalopiombi@gmail.com>
2021-08-19 11:35:13 -0300

    Add way of removing event listener on the grid after a mark is placed on it. Start coding logic to decide winner by rows or columns.

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ const gameBoard = (() => {
         for(let i = 0; i < 9; i++) {
             _createDiv();
             newDiv.classList.add('square');
+            newDiv.setAttribute('data-index', i);
             document.querySelector('.grid-container').appendChild(newDiv);
         }
     }
@@ -16,22 +17,26 @@ const gameBoard = (() => {
 
 const game = (() => {
     let moves = [];
-    let playerTurn;
+    let playerMove;
+    const rows = [0, 0, 0];
+    const columns = [0, 0, 0];
 
     const addClickEvent = () => {
         document.querySelectorAll('.square').forEach(div => {
-            div.addEventListener('click', (e) => {
+            div.addEventListener('click', function clicked(e) {
                 squareClicked = e.currentTarget;
+                squareClicked.removeEventListener(e.type, clicked);
                 _makeMark(squareClicked);
+                _checkWinner(e.currentTarget.dataset.index);
             });
         });
     }
-
+  
     const _checkPlayerTurn = () => {
         if (moves.length === 0 || moves[moves.length - 1] === 'o') {
-            return playerTurn = player1.mark;
+            return playerMove = player1.mark;
         } else {
-            return playerTurn = player2.mark;
+            return playerMove = player2.mark;
         }
     }
 
@@ -40,12 +45,47 @@ const game = (() => {
         if (square.textContent !== '') {
             return;
         } else {
-            square.textContent = playerTurn;
-            moves.push(playerTurn);
+            square.textContent = playerMove;
+            moves.push(playerMove);
         }
     }
 
-    return { addClickEvent };
+    const _checkWinner = squareIndex => {
+        squareIndex = Number(squareIndex);
+        const grid = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8]
+        ];
+
+        for(let i = 0; i < grid.length; i++) {
+            if (grid[i].indexOf(squareIndex) !== -1) {
+                if (playerMove === 'x') {
+                    rows[i] += 1;
+                    console.log(rows);
+                }
+                else {
+                    rows[i] -= 1;
+                    console.log(rows);
+                }
+            }
+        }
+
+        for(let i = 0; i < grid.length; i++) {
+            if (grid[i].indexOf(squareIndex) !== -1) {
+                if (playerMove === 'x') {
+                    columns[grid[i].indexOf(squareIndex)] += 1;
+                    console.log(columns);
+                }
+                else {
+                    columns[grid[i].indexOf(squareIndex)] -= 1;
+                    console.log(columns);
+                }
+            }
+        }
+    }
+
+    return { addClickEvent, _checkWinner };
 })();
 
 const player1 = player('Player 1', 'x');

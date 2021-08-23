c5c55f4c9eb79b03171f99228aa7aeee134f5dbb
Gonzalo <gonzalopiombi@gmail.com>
2021-08-23 14:02:09 -0300

    Restructure gameBoard module. Now the grid is not generated from JS. Simplify checkTie function.

--- script.js
+++ script.js
@@ -3,16 +3,14 @@ const player = (name, mark) => {
 }
 
 const gameBoard = (() => {
-    const _createDiv = () => newDiv = document.createElement('div');
-    const createGrid = () => {
-        for(let i = 0; i < 9; i++) {
-            _createDiv();
-            newDiv.classList.add('square');
-            newDiv.setAttribute('data-index', i);
-            document.querySelector('.grid-container').appendChild(newDiv);
-        }
-    }
-    return { createGrid };
+    document.querySelectorAll('.square').forEach(div => {
+        div.addEventListener('click', e => {
+            console.log(e);
+            squareClicked = e.currentTarget;
+            game._makeMark(squareClicked);
+            game._checkWinner(e.currentTarget.dataset.index);
+        })
+    });
 })();
 
 const game = (() => {
@@ -23,20 +21,15 @@ const game = (() => {
     //'rows' and 'columns' arrays have 3 values each, each one representing one row (from top to bottom) and one column (from left to right).
     let rows = [0, 0, 0];
     let columns = [0, 0, 0];
+    //Each variable represents a square of the diagonals. Winning combinations would be squares 0, 4, 8 or 2, 4, 6.
+    const d1 = document.querySelectorAll('.square')[0];
+    const d2 = document.querySelectorAll('.square')[4];
+    const d3 = document.querySelectorAll('.square')[8];
+    const d4 = document.querySelectorAll('.square')[2];
+    const d5 = document.querySelectorAll('.square')[6];
 
     const restartButton = document.createElement('button');
     restartButton.textContent = 'Restart game';
-
-    const addClickEvent = () => {
-        document.querySelectorAll('.square').forEach(div => {
-            div.addEventListener('click', e => {
-                console.log(e);
-                squareClicked = e.currentTarget;
-                _makeMark(squareClicked);
-                _checkWinner(e.currentTarget.dataset.index);
-            })
-        });
-    }
 
     const _checkPlayerTurn = () => {
         if (moves.length === 0 || moves[moves.length - 1] === 'o') {
@@ -66,7 +59,6 @@ const game = (() => {
             [3, 4, 5],
             [6, 7, 8]
         ];
-
         //Iterate through the grid array until it finds the index of 'squareIndex'.
         //If the player was player1 (playerMove === 'x') it will add 1 to the corresponding row in the rows array.
         //It will subtract 1 if the player was player2 (playerMove === 'o')/ 
@@ -91,7 +83,6 @@ const game = (() => {
                 }
             }
         }
-
         //Check for 'squareIndex' in the grid array so the same process aplied to rows can be aplied to columns.
         for(let i = 0; i < grid.length; i++) {
             if (grid[i].indexOf(squareIndex) !== -1) {
@@ -113,20 +104,11 @@ const game = (() => {
                 }
             }
         }
-
         _checkDiagonals();
         _checkTie();
     }
 
     const _checkDiagonals = () => {
-        //Each variable represents a square of the diagonals.
-        //Winning combinations would be squares 0, 4, 8 or 2, 4, 6.
-        const d1 = document.querySelectorAll('.square')[0];
-        const d2 = document.querySelectorAll('.square')[4];
-        const d3 = document.querySelectorAll('.square')[8];
-        const d4 = document.querySelectorAll('.square')[2];
-        const d5 = document.querySelectorAll('.square')[6];
-
         if ((d1.textContent === d2.textContent && d1.textContent === d3.textContent) && (d1.textContent === 'x')) {
             alert('Player 1 wins');
             _gameOver();
@@ -134,7 +116,7 @@ const game = (() => {
             alert('Player 2 wins');
             _gameOver();
         }
-
+        
         if ((d4.textContent === d2.textContent && d4.textContent === d5.textContent) && (d4.textContent === 'x')) {
             alert('Player 1 wins');
             _gameOver();
@@ -146,16 +128,20 @@ const game = (() => {
 
     const _checkTie = () => {
         if (gameOver) return;
-        let counter = 0;
-        for (let i = 0; i < document.querySelectorAll('.square').length; i++) {
-            if (document.querySelectorAll('.square')[i].textContent !== '') {
-                counter++;
-            }
-        }
-        if (counter === 9) {
+        if (moves.length === 9) {
             alert('Tie!');
             _gameOver();
         }
+        // let counter = 0;
+        // for (let i = 0; i < document.querySelectorAll('.square').length; i++) {
+        //     if (document.querySelectorAll('.square')[i].textContent !== '') {
+        //         counter++;
+        //     }
+        // }
+        // if (counter === 9) {
+        //     alert('Tie!');
+        //     _gameOver();
+        // }
     }
 
     const _gameOver = () => {
@@ -171,10 +157,8 @@ const game = (() => {
         })
     }
 
-    return { addClickEvent };
+    return { _makeMark, _checkWinner };
 })();
 
 const player1 = player('Player 1', 'x');
 const player2 = player('Player 2', 'o');
-gameBoard.createGrid();
-game.addClickEvent();
